3e1ed66430990ce2bb600c3f3869764cae255d4e
razumeyka <33911265+razumeyka@users.noreply.github.com>
2018-02-08 17:06:00 +0200

    almost finished

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-$( '.burger, ul li' ).click( function() {
+$( '.burger' ).click( function() {
    if ( $( '.navbar' ).is( ':hidden' ) ) {
     $( '.navbar' ).show('slideToggle');
+    $( ".burger" ).addClass( "active" );
   } else {
     $( '.navbar' ).hide('slideToggle');
+    $( ".burger" ).removeClass( "active" );
   }
 });
